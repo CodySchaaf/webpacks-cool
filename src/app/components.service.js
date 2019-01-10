1f76b6d9af8a65911d25dee610856997e20eb226
CodySchaaf <schaaf@sigfig.com>
2019-01-10 11:02:53 -0800

    Lets move our image back into html no need to use js
    If you look in the npm run build file for main.js you will
    see the image is still included properly with the hashed version
    via the <img src=\"" + __webpack_require__(/*! ./assets/webpack.svg */ "./src/assets/webpack.svg") + "\" alt=\"\">
    code

--- src/app/components.service.js
+++ src/app/components.service.js
@@ -1,18 +1,9 @@
-import WebpackImage from "../assets/webpack.svg";
-
 export class ComponentService {
     constructor() {
-        this.imageBox = document.getElementById('image');
         this.numberOneInput = document.getElementById('numberOne');
         this.numberTwoInput = document.getElementById('numberTwo');
         this.addValuesButton = document.getElementById('addValues');
         this.resultDiv = document.getElementById('result');
-
-
-        const myImage = new Image();
-        myImage.src = WebpackImage;
-
-        this.imageBox.appendChild(myImage);
     }
 
     getInputs() {
