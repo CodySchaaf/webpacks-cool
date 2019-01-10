ae3ac6da47c9ab40f3337f751fbb3061ff8872d5
CodySchaaf <schaaf@sigfig.com>
2019-01-10 10:08:58 -0800

    Adding dev server
    Before we start cleaning things up lets make development a little easier.
    We will use the webpack-dev-server, which will allow us auto reloading of the
    page and we no longer have to rebuild ourselves to check for changes. We have nice source
    maps for debugging. You will notice it is much faster, and no longer needs a dist folder since code is
    kept in memory.

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
+    mode: 'development',
     entry: './src/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
