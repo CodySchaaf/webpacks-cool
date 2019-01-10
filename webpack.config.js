d44cad0196d9b2785fbfb0d4667152a4e957bde6
CodySchaaf <schaaf@sigfig.com>
2019-01-10 10:00:02 -0800

    Move cleaning dist code into webpack
    It is important to ensure we have a complete bundle in our dist folder
    to be shipt after the build. This ensures no stale code is in there and
    we can package up our dist folder for production. Now our js code is sort of
    a mess now, lets utilize webpack to start oganizing our code a little better.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Webpack is Cool'
         })
